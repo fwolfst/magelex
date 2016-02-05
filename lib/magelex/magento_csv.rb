7c90d5a3dc0c4ddb0cf6e4ac4b0e6dcd9031a5b2
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-02-05 16:20:31 +0100

    add MagentoCSV module and spec

---
+++ lib/magelex/magento_csv.rb
@@ -0,0 +1,67 @@
+require 'csv'
+
+module Magelex
+  module MagentoCSV
+    MONEY_FIELDS = ['Order Shipping', 'Order Grand Total',
+                    'Item Total', 'Item Tax']
+
+    CSV::Converters[:german_money_amount] = lambda do |value, info|
+      if MONEY_FIELDS.include? info[:header]
+        value.gsub(',', '.').to_f
+      else
+        value
+      end
+    end
+
+    CSV::Converters[:order_date] = lambda do |value, info|
+      if info[:header] == 'Order Date'
+        value[0..10]
+      else
+        value
+      end
+    end
+
+    # Reads file and returns lexware_bills
+    def self.read filename
+      self.parse(File.read filename)
+    end
+
+    # Creates a bill with basic information from csv line
+    def self.init_bill row
+      bill = Magelex::LexwareBill.new
+
+      # TODO: defining a attribute|colum- map would be nicer
+      bill.order_nr = row['Order Number']
+      bill.customer_name = row['Billing Name']
+      bill.country_code  = row['Shipping Country']
+      bill.date          = row['Order Date']
+
+      bill.status        = row['Order Status']
+
+      bill.shipping_cost = row['Order Shipping']
+      bill.total         = row['Order Grand Total']
+      bill
+    end
+
+    def self.parse string
+      bills = []
+      current_bill = Magelex::LexwareBill.new
+
+      CSV::parse(string, :headers => :first_row,
+                 converters: [:all, :german_money_amount]) do |row|
+        # Multiple rows (with same order_nr) define one order
+        # One order will be mapped to one bill
+        if current_bill.order_nr != row['Order Number']
+          current_bill = init_bill row
+        end
+
+        current_bill.add_item(row['Item Total'], row['Item Tax'], row['Item Name'])
+
+        if !bills.include? (current_bill)
+          bills << current_bill
+        end
+      end
+      bills
+    end
+  end
+end
---
+++ spec/magento_csv_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Magelex::MagentoCSV do
+  describe '#parse' do
+    it 'parses empty string' do
+      expect(Magelex::MagentoCSV.parse "").to eq([])
+    end
+    it 'parses orders exported via magento' do
+      bills = Magelex::MagentoCSV.parse(File.read("spec/data/test_data.csv"))
+      expect(bills.size).to eq(2)
+    end
+    it 'converts german format float fields' do
+      bills = Magelex::MagentoCSV.parse(File.read("spec/data/test_data.csv"))
+      expect(bills[0].total).to eq(79.05)
+      expect(bills[0].shipping_cost).to eq(4.16)
+    end
+    it 'converts date field' do
+      bills = Magelex::MagentoCSV.parse(File.read("spec/data/test_data.csv"))
+      expect(bills[0].date).to eq("01.01.2015 22:10:28")
+    end
+    it 'adds up totals' do
+      bills = Magelex::MagentoCSV.parse(File.read("spec/data/test_data.csv"))
+      expect(false).to eq(true)
+    end
+  end
+end
