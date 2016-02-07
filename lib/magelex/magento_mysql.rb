0431cc9a4d90562186c9fee7889699ac0ebf86bc
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-02-07 17:34:29 +0100

    add magento_mysql module and conf file

---
+++ lib/magelex/magento_mysql.rb
@@ -0,0 +1,27 @@
+require 'mysql2'
+
+module Magelex
+  module MagentoMYSQL
+    def self.update_dates mysqlconf, bills
+      @client = Mysql2::Client.new(:host => mysqlconf[:host],
+                                   :port => mysqlconf[:port],
+                                   :database => mysqlconf[:database],
+                                   :username => mysqlconf[:username],
+                                   :password => mysqlconf[:password])
+
+      in_statement = bills.map{|b| "'#{b.order_id}'"}.join(',')
+      query = "SELECT increment_id, created_at, updated_at, "\
+        "invoice_status_id  FROM sales_flat_invoice "\
+        "WHERE increment_id IN (#{in_statement});"
+
+      # Build up index TODO rubyfy
+      bill_idx = {}
+      bills.each {|b| bill_idx[b.order_id] = b}
+      results = @client.query(query)
+
+      results.each do |row|
+        bill_idx[row['increment_id']].date = row['created_at']
+      end
+    end
+  end
+end
