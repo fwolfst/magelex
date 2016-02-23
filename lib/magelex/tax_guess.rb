3c3a4545d0f2109327268b6688868ba3acc14a3d
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-02-23 07:35:30 +0100

    cleanup taxguesser a bit.

--- lib/magelex/tax_guess.rb
+++ lib/magelex/tax_guess.rb
@@ -1,5 +1,7 @@
 module Magelex
   module TaxGuess
+    # Guesses the tax category.
+    # Question is: how many percent of total is tax_amount.
     def self.guess(total, tax_amount)
       if total == 0 && tax_amount == 0
         return :empty_item
@@ -8,8 +10,14 @@ module Magelex
         raise "TaxGuess: Cannot guess tax of "\
           "#{total}/#{tax_amount} (#{total - total/tax_amount})"
       end
+
+      # net: netto, gro: gross/brutto
+      # gro_price = net_price + taxes         ## (1: net_price = gro_price - taxes)
+      # gro_price = net_price + net_price * tax_perce
+      # tax_perce = (gro_price - net_price) / net_price
+      # tax_perce = gro_price / net_price - 1 ## (see 1)
       percentage = total.to_f / (total - tax_amount) - 1
-      puts percentage
+
       case percentage
         when -0.01..0.01
           :tax0
