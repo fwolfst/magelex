dc02e425e1815842ef01a4514654e5efc3bf8bc6
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-02-23 13:49:41 +0100

    typo fix log out.

--- lib/magelex/magento_csv.rb
+++ lib/magelex/magento_csv.rb
@@ -40,10 +40,10 @@ module Magelex
 
       bill.shipping_cost = row['Order Shipping']
       if bill.shipping_cost == 12.6
-        Magelex::logger.info "Correcting shipping cost of #{bill.order_nr} (12.6 -> 15 / 1.19 €"
+        Magelex::logger.info "Correcting shipping cost of #{bill.order_nr} (12.6 -> 15 / 1.19 €)"
         bill.shipping_cost = 15 / 1.19
       elsif bill.shipping_cost == 4.15
-        Magelex::logger.info "Correcting shipping cost of #{bill.order_nr} (4.15 -> 4.95 / 1.19 €"
+        Magelex::logger.info "Correcting shipping cost of #{bill.order_nr} (4.15 -> 4.95 / 1.19 €)"
         bill.shipping_cost = 4.95 / 1.19
       end
       bill.total         = row['Order Grand Total']
